71422ff0cc0a6106921bd55ee5bf1565feefbed0
williamscelenac <will4298@email.sc.edu>
2016-11-17 17:25:42 -0500

    Update myrandom.h
    Fixed the asterisks from 79 to 80 characters, fixed the spacing with the parenthesis.

--- hw7/myrandom.h
+++ hw7/myrandom.h
@@ -1,4 +1,4 @@
-/******************************************************************************
+/*******************************************************************************
  * Header file for the random number generator.
  *
  * Author/copyright:  Duncan Buell
@@ -21,14 +21,14 @@ using namespace std;
 
 class MyRandom {
 public:
- MyRandom();
- MyRandom(unsigned seed);
- virtual ~MyRandom();
+ MyRandom ();
+ MyRandom (unsigned seed);
+ virtual ~MyRandom ();
 
- int RandomExponentialInt(double mean);
- double RandomNormal(double mean, double dev);
- double RandomUniformDouble(double lower, double upper);
- int RandomUniformInt(int lower, int upper);
+ int RandomExponentialInt (double mean);
+ double RandomNormal (double mean, double dev);
+ double RandomUniformDouble (double lower, double upper);
+ int RandomUniformInt (int lower, int upper);
 
 private:
  unsigned int seed_;
