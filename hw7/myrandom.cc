dbfd2edf0a7ac69ff3e0c9ae6a0f51ba5df1031e
williamscelenac <will4298@email.sc.edu>
2016-11-17 17:24:29 -0500

    Update myrandom.cc
    Fixed the asterisks from 79 to 80 characters, fixed the spacing with the parenthesis.

--- hw7/myrandom.cc
+++ hw7/myrandom.cc
@@ -2,45 +2,45 @@
 
 #include <map>
 
-/******************************************************************************
- *3456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789
+/*******************************************************************************
  * Class 'MyRandom' for random number generation.
  *
  * Author: Duncan A. Buell
  * Date last modified: 23 May 2016
 **/
 
-/******************************************************************************
+/*******************************************************************************
  * Constructor
 **/
-MyRandom::MyRandom() {
+
+MyRandom::MyRando () {
   seed_ = 1;
-  generator_.seed(seed_);
+  generator_.seed (seed_);
 }
 
-/******************************************************************************
+/*******************************************************************************
  * Constructor
 **/
-MyRandom::MyRandom(unsigned seed) {
+MyRandom::MyRandom (unsigned seed) {
   seed_ = seed;
-  generator_.seed(seed_);
+  generator_.seed (seed_);
 }
 
-/******************************************************************************
+/*******************************************************************************
  * Destructor
 **/
-MyRandom::~MyRandom() {
+MyRandom::~MyRandom () {
 }
 
-/******************************************************************************
+/*******************************************************************************
  * Accessors and Mutators
 **/
 
-/******************************************************************************
+/*******************************************************************************
  * General functions.
 **/
 
-/******************************************************************************
+/*******************************************************************************
  * Function 'RandomExponentialInt'.
  * This generates 'double' random numbers exponentially distributed with
  * parameter 'lambda'.
@@ -53,14 +53,15 @@ MyRandom::~MyRandom() {
  * Returns:
  *   the random number as an 'int' rounded from the 'double'
 **/
-int MyRandom::RandomExponentialInt(double lambda) {
-  assert(lambda >= 0.0);
-  std::exponential_distribution<double> distribution(lambda);
-  int r = round(distribution(generator_));
+
+int MyRandom::RandomExponentialInt (double lambda) {
+  assert (lambda >= 0.0);
+  std::exponential_distribution <double> distribution (lambda);
+  int r = round (distribution (generator_));
   return r;
 }
 
-/******************************************************************************
+/*******************************************************************************
  * Function 'RandomNormal'.
  * This generates 'double' random numbers normally distributed with
  * mean 'mean' and standard deviation 'dev'.
@@ -72,14 +73,15 @@ int MyRandom::RandomExponentialInt(double lambda) {
  * Returns:
  *   the random number as a 'double'
 **/
-double MyRandom::RandomNormal(double mean, double dev) {
-  assert(dev >= 0.0);
-  std::normal_distribution<double> distribution(mean, dev);
-  double r = distribution(generator_);
+
+double MyRandom::RandomNormal (double mean, double dev) {
+  assert (dev >= 0.0);
+  std::normal_distribution <double> distribution (mean, dev);
+  double r = distribution (generator_);
   return r;
 }
 
-/******************************************************************************
+/*******************************************************************************
  * Function 'RandomUniformDouble'.
  * This generates 'double' random numbers uniformly distributed from
  * 'lower' to 'upper' inclusive.
@@ -91,14 +93,15 @@ double MyRandom::RandomNormal(double mean, double dev) {
  * Returns:
  *   the random number as a 'double'
 **/
-double MyRandom::RandomUniformDouble(double lower, double upper) {
-  assert(lower <= upper);
-  std::uniform_real_distribution<double> distribution(lower, upper);
-  double r = distribution(generator_);
+
+double MyRandom::RandomUniformDouble (double lower, double upper) {
+  assert (lower <= upper);
+  std::uniform_real_distribution <double> distribution (lower, upper);
+  double r = distribution (generator_);
   return r;
 }
 
-/******************************************************************************
+/*******************************************************************************
  * Function 'RandomUniformInt'.
  * This generates 'int' random numbers uniformly distributed from
  * 'lower' to 'upper' inclusive.
@@ -110,9 +113,10 @@ double MyRandom::RandomUniformDouble(double lower, double upper) {
  * Returns:
  *   the random number as an 'int'
 **/
-int MyRandom::RandomUniformInt(int lower, int upper) {
-  assert(lower <= upper);
-  std::uniform_int_distribution<int> distribution(lower, upper);
-  int r = distribution(generator_);
+
+int MyRandom::RandomUniformInt (int lower, int upper) {
+  assert (lower <= upper);
+  std::uniform_int_distribution <int> distribution (lower, upper);
+  int r = distribution (generator_);
   return r;
 }
