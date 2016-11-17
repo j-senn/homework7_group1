7b64b66d0736525c22d7720f8e9a29065e02a1dc
williamscelenac <will4298@email.sc.edu>
2016-11-17 18:16:56 -0500

    Update onepct.h
    Fixed the asterisks from 79 to 80, fixed the spacing with the parenthesis.

--- hw7/onepct.h
+++ hw7/onepct.h
@@ -1,4 +1,4 @@
-/******************************************************************************
+/*******************************************************************************
  * Header for the 'OnePct' class
  *
  * Author/copyright:  Duncan Buell
@@ -31,28 +31,32 @@ static const string kDummyString = "dummystring";
 
 class OnePct {
 public:
-/******************************************************************************
+ 
+/*******************************************************************************
  * Constructors and destructors for the class. 
 **/
- OnePct();
- OnePct(Scanner& infile);
- virtual ~OnePct();
+ 
+ OnePct ();
+ OnePct (Scanner& infile);
+ virtual ~OnePct ();
 
-/******************************************************************************
+/*******************************************************************************
  * Accessors and Mutators.
 **/
- int GetExpectedVoters() const;
- int GetPctNumber() const;
+ 
+ int GetExpectedVoters () const;
+ int GetPctNumber () const;
 
 /******************************************************************************
  * General functions.
 **/
- void ReadData(Scanner& infile);
- void RunSimulationPct(const Configuration& config, MyRandom& random,
-                       ofstream& out_stream);
+ 
+ void ReadData (Scanner& infile);
+ void RunSimulationPct (const Configuration& config, MyRandom& random,
+                        ofstream& out_stream);
 
- string ToString();
- string ToStringVoterMap(string label, multimap<int, OneVoter> themap);
+ string ToString ();
+ string ToStringVoterMap (string label, multimap <int, OneVoter> themap);
 
 private:
  int    pct_expected_voters_ = kDummyInt;
@@ -65,24 +69,25 @@ private:
  int    pct_num_voters_ = kDummyInt;
  double wait_dev_seconds_;
  double wait_mean_seconds_;
- set<int> stations_to_histo_;
- vector<int> free_stations_;
- multimap<int, OneVoter> voters_backup_;
- multimap<int, OneVoter> voters_done_voting_;
- multimap<int, OneVoter> voters_pending_;
- multimap<int, OneVoter> voters_voting_;
+ set <int> stations_to_histo_;
+ vector <int> free_stations_;
+ multimap <int, OneVoter> voters_backup_;
+ multimap <int, OneVoter> voters_done_voting_;
+ multimap <int, OneVoter> voters_pending_;
+ multimap <int, OneVoter> voters_voting_;
 
-/******************************************************************************
+/*******************************************************************************
  * General private functions.
 **/
+ 
  void CreateVoters(const Configuration& config, MyRandom& random,
                    ofstream& out_stream);
- int DoStatistics(int iteration, const Configuration& config, 
-                  int station_count, map<int, int>& map_for_histo,
-       	   ofstream& out_stream);
+ int DoStatistics (int iteration, const Configuration& config, 
+                   int station_count, map <int, int>& map_for_histo,
+       	           ofstream& out_stream);
                    
- void ComputeMeanAndDev();
- void RunSimulationPct2(int stations);
+ void ComputeMeanAndDev ();
+ void RunSimulationPct2 (int stations);
 
 };
 
