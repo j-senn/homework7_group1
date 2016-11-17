40ad574f57b20431e2bbd3ec999a4dccfd751704
williamscelenac <will4298@email.sc.edu>
2016-11-17 17:15:40 -0500

    Update configuration.h
    Updated the asterisks from 79 to 80, fixed the spacing between the parenthesis.

--- hw7/configuration.h
+++ hw7/configuration.h
@@ -1,4 +1,4 @@
-/******************************************************************************
+/*******************************************************************************
  * Header for the 'Configuration' class
  *
  * Author/copyright:  Duncan Buell
@@ -24,21 +24,22 @@ static const int kDefaultSeed = 19;
 static const int kDummyConfigInt = -111;
 static const double kDummyConfigDouble = -22.22;
 
-class Configuration
-{
+class Configuration {
 public:
-/******************************************************************************
+/*******************************************************************************
  * Constructors and destructors for the class. 
 **/
- Configuration();
- virtual ~Configuration();
+ 
+ Configuration ();
+ virtual ~Configuration ();
 
-/******************************************************************************
+/*******************************************************************************
  * Accessors and Mutators.
 **/
-/******************************************************************************
+/*******************************************************************************
  * public variables
 **/
+ 
  int seed_ = kDefaultSeed;
  int election_day_length_hours_ = kDummyConfigInt;
  int election_day_length_seconds_ = kDummyConfigInt;
@@ -47,17 +48,17 @@ public:
  int min_expected_to_simulate_ = kDummyConfigInt;
  int wait_time_minutes_that_is_too_long_ = kDummyConfigInt;
  int number_of_iterations_ = kDummyConfigInt;
- vector<int> actual_service_times_;
+ vector <int> actual_service_times_;
  double arrival_zero_ = kDummyConfigDouble;
- vector<double> arrival_fractions_;
+ vector <double> arrival_fractions_;
 
-/******************************************************************************
+/*******************************************************************************
  * General functions.
 **/
 
- int GetMaxServiceSubscript() const;
- void ReadConfiguration(Scanner& instream);
- string ToString();
+ int GetMaxServiceSubscript () const;
+ void ReadConfiguration (Scanner& instream);
+ string ToString ();
 
 private:
 
