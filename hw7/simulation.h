3f966bd6b40110b454211c1f0009e3146b09a830
Celena Williams <will4298@email.sc.edu>
2016-11-19 16:32:09 -0500

    Update simulation.h
    Removed unnecessary spacing.

--- hw7/simulation.h
+++ hw7/simulation.h
@@ -26,18 +26,18 @@ public:
 /*******************************************************************************
  * Constructors and destructors for the class. 
 **/
-  Simulation ();
-  virtual ~Simulation ();
+  Simulation();
+  virtual ~Simulation();
 
 /*******************************************************************************
  * General functions.
 **/
   
-  void ReadPrecincts (Scanner& infile);
-  void RunSimulation (const Configuration& config, MyRandom& random,
-                      ofstream& out_stream);
-  string ToString ();
-  string ToStringPcts ();
+  void ReadPrecincts(Scanner& infile);
+  void RunSimulation(const Configuration& config, MyRandom& random,
+                     ofstream& out_stream);
+  string ToString();
+  string ToStringPcts();
 
 private:
   
@@ -45,7 +45,7 @@ private:
  * Variables.
 **/
   
-  map<int, OnePct> pcts_;
+  map<int, OnePct>pcts_;
 
 /*******************************************************************************
  * Private functions.
