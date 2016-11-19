633ab310f2d34784360e07e03742a3aed1e7fa64
Celena Williams <will4298@email.sc.edu>
2016-11-19 16:26:37 -0500

    Update main.cc
    Changed one last "+=" to ".append", removed unnecessary spacing.

--- hw7/main.cc
+++ hw7/main.cc
@@ -12,7 +12,7 @@
 
 static const string kTag = "MAIN: ";
 
-int main (int argc, char *argv []) {
+int main(int argc, char *argv[]){
   string config_filename;
   string pct_filename = kDummyFilename;
   string log_filename = kDummyFilename;
@@ -33,23 +33,23 @@ int main (int argc, char *argv []) {
   cout<< kTag << "Beginning execution" << endl;
   
 
-  Utils::CheckArgs (4, argc, argv,
-                    "configfilename pctfilename outfilename logfilename");
-  config_filename = static_cast <string> (argv [1]);
-  pct_filename = static_cast <string> (argv [2]);
-  out_filename = static_cast <string> (argv [3]);
-  log_filename = static_cast <string> (argv [4]);
+  Utils::CheckArgs(4, argc, argv,
+                   "configfilename pctfilename outfilename logfilename");
+  config_filename = static_cast<string>(argv[1]);
+  pct_filename = static_cast<string>(argv[2]);
+  out_filename = static_cast<string>(argv[3]);
+  log_filename = static_cast<string>(argv[4]);
 
-  Utils::FileOpen (out_stream, out_filename);
-  Utils::LogFileOpen (log_filename);
+  Utils::FileOpen(out_stream, out_filename);
+  Utils::LogFileOpen(log_filename);
 
   outstring = kTag + "Beginning execution\n";
-  outstring.append (kTag + Utils::TimeCall("beginning"));
+  outstring.append(kTag + Utils::TimeCall("beginning"));
   out_stream << outstring << endl;
   Utils::log_stream << outstring << endl;
 
   outstring = kTag + "outfile '" + out_filename + "'" + "\n";
-  outstring.append (kTag + "logfile '" + log_filename + "'" + "\n");
+  outstring.append(kTag + "logfile '" + log_filename + "'" + "\n");
   out_stream << outstring << endl;
   Utils::log_stream << outstring << endl;
 
@@ -58,41 +58,41 @@ int main (int argc, char *argv []) {
    * and mean and dev voting time
  **/
   
-  config_stream.OpenFile (config_filename);
-  config.ReadConfiguration (config_stream);
-  config_stream.Close ();
+  config_stream.OpenFile(config_filename);
+  config.ReadConfiguration(config_stream);
+  config_stream.Close();
 
-  outstring = kTag + config.ToString () + "\n";
+  outstring = kTag + config.ToString() + "\n";
   out_stream << outstring << endl;
   Utils::log_stream << outstring << endl;
 
-  random = MyRandom (config.seed_);
+  random = MyRandom(config.seed_);
 
   /*******************************************************************************
    * now read the precinct data
  **/
   
-  pct_stream.OpenFile (pct_filename);
-  simulation.ReadPrecincts (pct_stream);
-  pct_stream.Close ();
+  pct_stream.OpenFile(pct_filename);
+  simulation.ReadPrecincts(pct_stream);
+  pct_stream.Close();
 
   /*******************************************************************************
    * here is the real work
  **/
   
-  simulation.RunSimulation (config, random, out_stream);
+  simulation.RunSimulation(config, random, out_stream);
 
   /*******************************************************************************
    * close up and go home
   **/
   
   outstring = kTag + "Ending execution" + "\n";
-  outstring += kTag + Utils::TimeCall ("ending");
+  outstring.append(kTag + Utils::TimeCall("ending"));
   out_stream << outstring << endl;
   Utils::log_stream << outstring << endl;
 
-  Utils::FileClose (out_stream);
-  Utils::FileClose (Utils::log_stream);
+  Utils::FileClose(out_stream);
+  Utils::FileClose(Utils::log_stream);
 
   cout<< kTag << "Ending execution" << endl;
 
