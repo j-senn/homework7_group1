26e5cede3812e676c0572e0011dc6f13afa34f88
Spence14 <scs2@email.sc.edu>
2016-11-29 14:47:26 -0500

    Updated copyright information

--- hw7/main.h
+++ hw7/main.h
@@ -4,6 +4,11 @@
  * Author/copyright:  Duncan Buell
  * Date: 6 October 2016
  *
+ * Used with permission by: Group 1 (BRYCE ANDREW BLANTON,
+ * WILLIAM R EDWARDS, JONATHAN SENN, SPENCER C SMITH,
+ * CELENA CHRISTINE WILLIAMS)
+ * Date last modified: 1 December 2016
+ *
 **/
 
 #ifndef MAIN_H
