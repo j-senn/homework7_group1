699bf3cf30aa89e4767f9a81eb8c686e28011f63
Spence14 <scs2@email.sc.edu>
2016-11-29 14:50:11 -0500

    Updated copyright information

--- hw7/onepct.h
+++ hw7/onepct.h
@@ -5,6 +5,11 @@
  * TODO update copyright
  * Date: 6 October 2016
  *
+ * Used with permission by: Group 1 (BRYCE ANDREW BLANTON,
+ * WILLIAM R EDWARDS, JONATHAN SENN, SPENCER C SMITH,
+ * CELENA CHRISTINE WILLIAMS)
+ * Date last modified: 1 December 2016
+ *
 **/
 
 #ifndef ONEPCT_H
